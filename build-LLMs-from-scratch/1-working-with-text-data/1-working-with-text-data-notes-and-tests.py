0abf532536f6c0744158d635075163bca7112175
Daniele Berardi <daniele99.berardi@gmail.com>
2025-05-29 07:08:23 +0200

    [build-LLMs-from-scratch] Second part of Sebastian Raschka course about text embeddings for LLMs

--- build-LLMs-from-scratch/1-working-with-text-data/1-working-with-text-data-notes-and-tests.py
+++ build-LLMs-from-scratch/1-working-with-text-data/1-working-with-text-data-notes-and-tests.py
@@ -36,10 +36,16 @@ encode into vectors. Thus, our goal is going from letters and words into a numer
 import os # needed for working with files
 import urllib.request # needed for downloading stuffs from the internet
 import re # regular expressions
-from tokenizer import SimpleTokenizerV1, SimpleTokenizerV2
+from tokenizer import SimpleTokenizerV1, SimpleTokenizerV2, GPTDatasetV1, create_dataloader_v1
 import tiktoken # we will use the byte pair algorithm (BPE) implemented by OpenAI's tiktoken API
+import torch
 
-# 1. tokenizing text starting from an input text (step 1 of text-embedding-workflow.png)
+"""
+----------
+1) Tokenizing text starting from an input text (step 1 of text-embedding-workflow.png).
+----------
+"""
+
 if not os.path.exists("the-verdict.txt"):
     url = ("https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/refs/heads/main/ch02/01_main-chapter-code/the-verdict.txt")
     file_path = "the-verdict.txt"
@@ -47,73 +53,129 @@ if not os.path.exists("the-verdict.txt"):
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
+print("---")
 print(len(raw_text))
 
 text = "Hello, world. This, is a test."
 result = re.split(r'(\s)', text) # it splits text into individual tokens whenever a whitespace is found
+print("---")
 print(result)
 
 text = "Hello, world. Is this-- a test?"
 result = re.split(r'([,.]|\s)', text) # it splits text whenever it finds whitspaces/punctuation
+print("---")
 print(result)
 result = [item for item in result if item.strip()] # removing blank tokens (the "strip" method removes spaces in the string)
+print("---")
 print(result) # this was the simplest way of tokenizing text
 
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text) # using regular expression on our example
 preprocessed = [item for item in preprocessed if item.strip()]
+print("---")
 print(preprocessed[:30])
 print(len(preprocessed)) # we have 4690 tokens!
 
-# 2. converting tokens into token IDs (step 2 of text-embedding-workflow.png)
-# the first step is to build a vocabulary, namely a unique mapping between each word that can occur and a unique integer
-# once that we have a vocabulary, we can convert tokens into token IDs
+"""
+----------
+2) Converting tokens into token IDs (step 2 of text-embedding-workflow.png). The first step is to build a vocabulary, namely a
+unique mapping between each word that can occur and a unique integer. Once that we have a vocabulary, we can convert tokens
+into token IDs.
+----------
+"""
+
 all_words = sorted(set(preprocessed)) # we can get rid of duplicates in our tokens
 vocab_size = len(all_words)
+print("---")
 print(all_words[:30])
 print(vocab_size)
 
 vocab = {token: integer for integer, token in enumerate(all_words)}
+print("---")
 print(vocab)
 
 tokenizer = SimpleTokenizerV1(vocab) # we have already created the vocabulary
 text = """"It's the last he painted, you know," Mrs. Gisburn said with pardonable pride.""" # this is an extract from the-verdict.txt
 ids = tokenizer.encode(text) # these are the token IDs of the above text
+print("---")
 print(ids)
 print(tokenizer.decode(ids))
 
-# 3. adding special context tokens (we want to extend our vocabulary with special tokens...)
-# some of these special tokens could be useful for unknown words (namely words that are not in the vocabulary) or for the end of text
+"""
+----------
+3) Adding special context tokens (we want to extend our vocabulary with special tokens...). Some of these special tokens could be
+useful for unknown words (namely words that are not in the vocabulary) or for the end of text.
+----------
+"""
+
 text = "Hello, do you like tea. is this-- a text?"
 # tokenizer.encode(text) # it gives a KeyError since the word "Hello" is not included in our vocabulary
 
 all_tokens = sorted(list(set(preprocessed)))
 all_tokens.extend(["<|endoftext|>", "<|unk|>"])
 vocab = {token: integer for integer, token in enumerate(all_tokens)} # we re-generate the vocabulary as before
+print("---")
 print(vocab)
 tokenizer = SimpleTokenizerV2(vocab) # we use the new tokenizer with the new vocabulary
 ids = tokenizer.encode(text)
+print("---")
 print(ids)
 print(tokenizer.decode(ids)) # here we won't see the unknown words
 
-# 4. using byte pair enconding to handle unknown tokens
-# byte pair enconding (BPE) is an algorithm used by GPT-1, GPT-2, etc. tokenizers to deal with unknown tokens
-# for example, tiktokenizer (https://tiktokenizer.vercel.app/?model=gpt2) splits the unknown word in two or more (known) subwords
-# based on the training data used for the particular model (GPT-1, GPT-2, etc.)
-# check this link https://github.com/openai/gpt-2/blob/master/src/encoder.py to see how GPT-2 has implemented the BPE algorithm
+"""
+----------
+4) Using Byte Pair Enconding algorithm to handle unknown tokens. Byte Pair Enconding (BPE) is an algorithm used by GPT-1, GPT-2, etc.
+tokenizers to deal with unknown tokens. For example, tiktokenizer (https://tiktokenizer.vercel.app/?model=gpt2) splits the unknown
+word in two or more (known) subwords based on the training data used for the particular model (GPT-1, GPT-2, etc.).
+Check this link https://github.com/openai/gpt-2/blob/master/src/encoder.py to see how GPT-2 has implemented the BPE algorithm.
+----------
+"""
+
 tokenizer = tiktoken.get_encoding("gpt2") # we instantiate a new tokenizer with its full vocabulary, the way it was trained, etc.
+print("---")
 print(tokenizer.encode("Hello world"))
 print(tokenizer.decode(tokenizer.encode("Hello world")))
 
 text = ("Hello, do you like tea? <|endoftext|> In the sunlit terraces of someunknownPlace "
         "jsahsjhjshSAK<SJAKshA") # the <|endoftext|> special token is used to signal to the LLM where a document ends and another starts
+print("---")
 print(tokenizer.encode(text, allowed_special={"<|endoftext|>"}))
 print(tokenizer.decode(tokenizer.encode(text, allowed_special={"<|endoftext|>"})))
 
-# 5. data sampling with a sliding window (how do we provide smaller chunks of these token IDs to the LLM efficiently?)
+"""
+----------
+5) Data sampling with a sliding window. How do we provide smaller chunks of these token IDs to the LLM to train it efficiently?
+For example, GPT-2 uses a chunk size of 1024, while GPT-4 of 8,192! Here it will be used a chunk size of 4 for demonstration purpose.
+----------
+"""
+
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
 enc_text = tokenizer.encode(raw_text) # tokenize our training text using tiktoken's tokenizer
-#print(len(enc_text)) # how to provide these 5145 token IDs efficently (as sub-chunks) to our LLM?
+print("---")
+print(len(enc_text)) # how to provide these 5145 token IDs efficently (as sub-chunks) to our LLM?
 
+enc_sample = enc_text[50:] # we truncate the first fifty characters
+print("---")
+print(len(enc_sample))
+context_size = 4 # chunk size: this is the size of our input (each input will contain four tokens)
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size]
+print("---")
+print(f"x: {x}")
+print(f"y:      {y}")
 
+for i in range(1, context_size+1): # i = 1, 2, 3, 4
+    context = enc_sample[:i] # what we give as input to the LLM
+    desired = enc_sample[i] # what we except as output from the LLM
+    print("---")
+    print(f"TOKENS: {context} ----> {desired}")
+    print(f"WORDS: {tokenizer.decode(context)} ----> {tokenizer.decode([desired])}") # "decode" method takes a list as input
+
+# using "Dataset" and "DataLoader" classes from PyTorch ("torch") library to create our chunks of data (each made of four tokens)
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+print("---")
+print(raw_text)
+
+dataloader = create_dataloader_v1(raw_text, batch_size = 4, max_length = 256, stride = 1, shuffle = False)
--- build-LLMs-from-scratch/1-working-with-text-data/tokenizer.py
+++ build-LLMs-from-scratch/1-working-with-text-data/tokenizer.py
@@ -1,83 +1,109 @@
 import re
 import json
+import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
+from tiktoken import Encoding
 
-class SimpleTokenizerV1: # this is not the real tokenizer used by GPT models, but it is a simplified version
-    '''
+class SimpleTokenizerV1:  # this is not the real tokenizer used by GPT models, but it is a simplified version
+    """
     A first simplified version of tokenizer.
-    '''
-    def __init__(self, vocab: dict) -> None: # the class takes a vocabulary as input
-        self.str_to_int = vocab # regular vocabulary (string to integer mapping)
-        self.int_to_str = {i: s for s, i in vocab.items()} # inverted vocabulary (integer to string mapping)
+    """
+
+    def __init__(self, vocab: dict) -> None:  # the class takes a vocabulary as input
+        self.str_to_int = vocab  # regular vocabulary (string to integer mapping)
+        self.int_to_str = {
+            i: s for s, i in vocab.items()
+        }  # inverted vocabulary (integer to string mapping)
 
     def encode(self, text: str) -> list[int]:
-        '''
+        """
         A function to break down text into tokens using regular expressions.
-        '''
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text) # creation of tokenized version of input text
+        """
+        preprocessed = re.split(
+            r'([,.:;?_!"()\']|--|\s)', text
+        )  # creation of tokenized version of input text
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
-        ids = [self.str_to_int[s] for s in preprocessed] # creation of token ID for each token/string
+        ids = [
+            self.str_to_int[s] for s in preprocessed
+        ]  # creation of token ID for each token/string
         return ids
-    
+
     def decode(self, ids: list[int]) -> str:
-        '''
+        """
         A function to re-create the original text starting from the token IDs.
-        '''
+        """
         text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text) # replace spaces before the specified punctuations
+        text = re.sub(
+            r'\s+([,.?!"()\'])', r"\1", text
+        )  # replace spaces before the specified punctuations
         return text
 
-class SimpleTokenizerV2: # advanced version of our tokenizer including special tokens
-    '''
+
+class SimpleTokenizerV2:  # advanced version of our tokenizer including special tokens
+    """
     A second version of tokenizer which is also able to deal with unknown words.
-    '''
-    def __init__(self, vocab: dict) -> None: # the class takes a vocabulary as input
-        self.str_to_int = vocab # regular vocabulary (string to integer mapping)
-        self.int_to_str = {i: s for s, i in vocab.items()} # inverted vocabulary (integer to string mapping)
+    """
+
+    def __init__(self, vocab: dict) -> None:  # the class takes a vocabulary as input
+        self.str_to_int = vocab  # regular vocabulary (string to integer mapping)
+        self.int_to_str = {
+            i: s for s, i in vocab.items()
+        }  # inverted vocabulary (integer to string mapping)
 
     def encode(self, text: str) -> list[int]:
-        '''
+        """
         A function to break down text into tokens using regular expressions and including special tokens.
-        '''
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text) # creation of tokenized version of input text
+        """
+        preprocessed = re.split(
+            r'([,.:;?_!"()\']|--|\s)', text
+        )  # creation of tokenized version of input text
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
-        preprocessed = [item if item in self.str_to_int else "<|unk|>"
-                        for item in preprocessed] # replace the string if it isn't in the vocabulary
-        ids = [self.str_to_int[s] for s in preprocessed] # creation of token ID for each token/string
+        preprocessed = [
+            item if item in self.str_to_int else "<|unk|>" for item in preprocessed
+        ]  # replace the string if it isn't in the vocabulary
+        ids = [
+            self.str_to_int[s] for s in preprocessed
+        ]  # creation of token ID for each token/string
         return ids
-    
+
     def decode(self, ids: list[int]) -> str:
-        '''
+        """
         A function to re-create the original text starting from the token IDs.
-        '''
+        """
         text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text) # replace spaces before the specified punctuations
+        text = re.sub(
+            r'\s+([,.?!"()\'])', r"\1", text
+        )  # replace spaces before the specified punctuations
         return text
+
 
 class BPETokenizerSimple:
-    '''
+    """
     A simplified version of the BPE (Byte Pair Encoding) tokenizer by Sebastian Raschka.
 
     Links:
         https://github.com/rasbt/LLMs-from-scratch/blob/main/ch02/05_bpe-from-scratch/bpe-from-scratch.ipynb
         https://sebastianraschka.com/blog/2025/bpe-from-scratch.html
-    '''
+    """
+
     def __init__(self) -> None:
-        self.vocab = {} # map token_id to token_str (e.g., {11246: "some"})
-        self.inverse_vocab = {} # map token_str to token_id (e.g., {"some": 11246})
-        self.bpe_merges = {} # dictionary of BPE merges/pairs (e.g., {(token_id1, token_id2): merged_token_id})
-        self.bpe_ranks = {} # TODO
+        self.vocab = {}  # map token_id to token_str (e.g., {11246: "some"})
+        self.inverse_vocab = {}  # map token_str to token_id (e.g., {"some": 11246})
+        self.bpe_merges = {}  # dictionary of BPE merges/pairs (e.g., {(token_id1, token_id2): merged_token_id})
+        self.bpe_ranks = {}  # TODO
 
     def encode(self, text: str, allowed_special: set = None) -> list[int]:
-        '''
+        """
         Encode the input text into a list of token IDs.
 
         Args:
             text (str): The input text to encode.
             allowed_special (set or None): Special tokens to allow passthrough.
-        
+
         Returns:
             token_ids (list[int]): List of token IDs.
-        '''
+        """
         import re
 
         token_ids = []
@@ -86,29 +112,41 @@ class BPETokenizerSimple:
 
         # If no special tokens are present
         tokens = []
-        lines = text.split("\n") # break the string at each occurrence of the "\n" character
+        lines = text.split(
+            "\n"
+        )  # break the string at each occurrence of the "\n" character
         for i, line in enumerate(lines):
             if i > 0:
-                tokens.append("\n") # only from the second line onwards we append the "\n"
-            words = line.split() # split the string with each occurrence of whitespace
+                tokens.append(
+                    "\n"
+                )  # only from the second line onwards we append the "\n"
+            words = line.split()  # split the string with each occurrence of whitespace
             for j, word in enumerate(words):
-                if j == 0 and i > 0: # replace whitespaces with "Ġ" (e.g., "Hello world" might be tokenized as ["Hello", "Ġworld"])
-                    tokens.append("Ġ" + word) # also for the first word of each new line from the second onwards we append the "Ġ"
+                if (
+                    j == 0 and i > 0
+                ):  # replace whitespaces with "Ġ" (e.g., "Hello world" might be tokenized as ["Hello", "Ġworld"])
+                    tokens.append(
+                        "Ġ" + word
+                    )  # also for the first word of each new line from the second onwards we append the "Ġ"
                 elif j == 0:
-                    tokens.append(word) # if there is only one line, the first word (j == 0) is appended without the "Ġ"
+                    tokens.append(
+                        word
+                    )  # if there is only one line, the first word (j == 0) is appended without the "Ġ"
                 else:
-                    tokens.append("Ġ" + word) # from the second word onwards we append the "Ġ" to show the presence of a whitespace
-        
-        for token in tokens: # "token" is a string here
+                    tokens.append(
+                        "Ġ" + word
+                    )  # from the second word onwards we append the "Ġ" to show the presence of a whitespace
+
+        for token in tokens:  # "token" is a string here
             if token in self.inverse_vocab:
                 token_ids.append(self.inverse_vocab[token])
             else:
                 token_ids.extend(self.tokenize_with_bpe(token))
-        
+
         return token_ids
-    
+
     def tokenize_with_bpe(self, token: str) -> list[int]:
-        '''
+        """
         Tokenize a single token using BPE merges.
 
         Args:
@@ -116,21 +154,27 @@ class BPETokenizerSimple:
 
         Returns:
             token_ids (list[int]): The list of token IDs after applying BPE.
-        '''
+        """
         # tokenization starts always by splitting each word into its characters
-        token_ids = [self.inverse_vocab.get(char, None) for char in token] # tokenize the token into individual characters
+        token_ids = [
+            self.inverse_vocab.get(char, None) for char in token
+        ]  # tokenize the token into individual characters
         if None in token_ids:
             missing_chars = [char for char, tid in zip(token, token_ids) if tid is None]
             raise ValueError(f"Characters not found in vocab: {missing_chars}")
-        
+
         # if OpenAI's GPT-2 merges have not been loaded
         if not self.bpe_ranks:
             can_merge = True
-            while can_merge and len(token_ids) > 1: # token_ids contains the IDs of the word's single characters
+            while (
+                can_merge and len(token_ids) > 1
+            ):  # token_ids contains the IDs of the word's single characters
                 can_merge = False
                 new_tokens = []
                 i = 0
-                while i < len(token_ids) - 1: # scan through the entire length of the word
+                while (
+                    i < len(token_ids) - 1
+                ):  # scan through the entire length of the word
                     pair = (token_ids[i], token_ids[i + 1])
                     if pair in self.bpe_merges:
                         merged_token_id = self.bpe_merges[pair]
@@ -142,17 +186,17 @@ class BPETokenizerSimple:
                         i += 1
                 if i < len(token_ids):
                     new_tokens.append(token_ids[i])
-                token_ids = new_tokens # once we substitute the pairs, we loop again to find new pairs
+                token_ids = new_tokens  # once we substitute the pairs, we loop again to find new pairs
             return token_ids
-    
+
     def load_vocab_and_merges(self, vocab_path: str, bpe_merges_path: str) -> None:
-        '''
+        """
         Load the vocabulary and BPE merges from JSON files.
 
         Args:
             vocab_path (str): Path to the vocabulary (JSON) file.
             bpe_merges_path (str): Path to the BPE merges (text) file.
-        '''
+        """
         # load vocabulary
         with open(vocab_path, "r", encoding="utf-8") as file:
             loaded_vocab = json.load(file)
@@ -161,23 +205,34 @@ class BPETokenizerSimple:
 
         # handle newline character without adding a new token (first prio for "fallback_token" is "<|endoftext|>")
         if "\n" not in self.inverse_vocab:
-            fallback_token = next((token for token in ["<|endoftext|>", "Ġ", ""] if token in self.inverse_vocab), None)
+            fallback_token = next(
+                (
+                    token
+                    for token in ["<|endoftext|>", "Ġ", ""]
+                    if token in self.inverse_vocab
+                ),
+                None,
+            )
             if fallback_token is not None:
-                newline_token_id = self.inverse_vocab[fallback_token] # assign <|endoftext|>'s token ID to \n's one
+                newline_token_id = self.inverse_vocab[
+                    fallback_token
+                ]  # assign <|endoftext|>'s token ID to \n's one
             else:
                 raise KeyError("No suitable token found in vocabulary to map '\\n'.")
-            
+
             self.inverse_vocab["\n"] = newline_token_id
             self.vocab[newline_token_id] = "\n"
-        
+
         # load BPE merges
         with open(bpe_merges_path, "r", encoding="utf-8") as file:
             lines = file.readlines()
             if lines and lines[0].startswith("#"):
-                lines = lines[1:] # skip header line if present
-            
+                lines = lines[1:]  # skip header line if present
+
             for rank, line in enumerate(lines):
-                pair = tuple(line.strip().split()) # .strip() removes the whitespaces before and after, while .split() divides the two pairs
+                pair = tuple(
+                    line.strip().split()
+                )  # .strip() removes the whitespaces before and after, while .split() divides the two pairs
                 if len(pair) != 2:
                     print(f"Line {rank + 1} has more than 2 entries: {line.strip()}")
                     continue
@@ -190,18 +245,121 @@ class BPETokenizerSimple:
                         merged_token_id = self.inverse_vocab[merged_token]
                         self.bpe_merges[(token_id1, token_id2)] = merged_token_id
                     else:
-                        print(f"Merged token '{merged_token}' not found in vocab. Skipping.")
+                        print(
+                            f"Merged token '{merged_token}' not found in vocab. Skipping."
+                        )
                 else:
-                    print(f"Skipping pair {pair} as one of the tokens is not in the vocabulary.")
-    
-    def train(self, text: str, vocab_size: int, allowed_special: set = {"<|endoftext|>"}) -> None:
-        '''
+                    print(
+                        f"Skipping pair {pair} as one of the tokens is not in the vocabulary."
+                    )
+
+    def train(
+        self, text: str, vocab_size: int, allowed_special: set = {"<|endoftext|>"}
+    ) -> None:
+        """
         Train the BPE tokenizer from scratch (without using any existing vocabulary).
 
         Args:
             text (str): The training text.
             vocab_size (int): The desired vocabulary size.
             allowed_special (set): A set of special tokens to include in the vocabulary.
-        '''
+        """
         # TODO
-        
+
+
+class GPTDatasetV1(Dataset):
+    """
+    Create the dataset (namely input and target chunks) needed to pre-train the LLM.
+
+    Args:
+        txt (str): The training dataset (the text to tokenize and to divide into chunks).
+        tokenizer (Encoding): The tokenizer to use.
+        max_length (int): The context's length (namely the chunk size).
+        stride (int): ...
+    """
+
+    def __init__(self, txt: str, tokenizer: Encoding, max_length: int, stride: int) -> None:
+        """
+        Link: https://github.com/openai/tiktoken/blob/4560a8896f5fb1d35c6f8fd6eee0399f9a1a27ca/tiktoken/registry.py#L63
+        """
+        self.input_ids = []
+        self.target_ids = []
+
+        # tokenize the entire text
+        token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+
+        # use a sliding window to chunk the book into overlapping seuences of max_length
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i : i + max_length]
+            target_chunk = token_ids[
+                i + 1 : i + max_length + 1
+            ]  # target = input shifted by 1 position
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self) -> int:
+        """
+        Return the total number of rows in the dataset.
+
+        Returns:
+            len(self.input_ids) (int): The total number of rows in the dataset.
+        """
+        return len(self.input_ids)
+
+    def __getitem__(self, idx) -> list[int]:
+        """
+        Return a single row from the dataset.
+
+        Args:
+            idx (int): The row index.
+
+        Returns:
+            self.input_ids[idx] (list[int]): The input row from the dataset.
+            self.target_ids[idx] (list[int]): The target row from the dataset.
+        """
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader_v1(
+    txt: str,
+    batch_size: int = 4,
+    max_length: int = 256,
+    stride: int = 128,
+    shuffle: bool = True,
+    drop_last: bool = True,
+    num_workers: int = 0,
+) -> DataLoader:
+    """
+    Write here and add type hints to the function ...
+
+    Args:
+        txt (str): The training dataset.
+        batch_size (int): The number of samples are in each batch.
+        max_length (int): The context's length (namely the chunk size). 
+        stride (int): ...
+        shuffle (bool): ...
+        drop_last (bool): If true it truncates the last batch (chunk) whenever the dataset size is not
+        divisible by the batch_size. Leaving this last (small) batch could result in a loss spike and
+        thus in an unstable training. It is usually a good idea to have batches of the same size.
+        num_workers (int): It is for using multiple background processors (multiple subprocesses).
+
+    Returns:
+        dataloader (DataLoader): ...
+    """
+
+    # initialize the (tiktoken's) tokenizer
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    # create dataset
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+
+    # create dataloader
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+
+    return dataloader
